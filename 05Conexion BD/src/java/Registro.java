a4b3df168382e96824dec55ec453034fe10a1149
LuisDavid2003 <l.raya.m457@edu.utc.mx>
2024-03-11 18:51:00 -0600

    Conexion BD

--- 05Conexion BD/src/java/Registro.java
+++ 05Conexion BD/src/java/Registro.java
@@ -106,20 +106,21 @@ public class Registro extends HttpServlet {
                 System.out.println(e.getStackTrace());
             }
             
-            /* TODO output your page here. You may use following sample code. */
+              /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
             out.println("<title>Servlet Registro</title>");            
             out.println("</head>");
             out.println("<body>");
-         
+            out.println("<h1>Registro Exitoso wiiii</h1>");
+            /* TODO output your page here. You may use following sample code. */
+ 
             out.println("<a href='index.html' >Regresar al Menu Principal</a>");
             out.println("</body>");
             out.println("</html>");
         }
     }
-
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -134,29 +135,4 @@ public class Registro extends HttpServlet {
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
